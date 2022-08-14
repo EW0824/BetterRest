75e1cfce8c0f410ddfa70ab2bc122c3dbcec0b16
Edmond <34758460+EdmondtheGreat@users.noreply.github.com>
2022-08-14 18:04:36 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by OAA on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
